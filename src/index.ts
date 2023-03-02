635fc9870dc3a3a8886fdae346351cd3bbbfee60
Carlos Garcia <carlos@soltechnologies.es>
2023-03-02 09:23:57 +0100

    change encryption method to asymmetic public/private keys, using JSEncrypt

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,15 @@
-import CryptoJS from 'crypto-js';
+import JSEncrypt from 'jsencrypt';
 
-let publicKey: string;
-let encryptionKey: string;
-
-export const generatePublicKey = (generatorBase: number, privateKey: number, bigPrime: number) => {
-  publicKey = expmod(generatorBase, privateKey, bigPrime).toString();
+export const encryptData = (publicKey: string, data: string): string => {
+    const crypt = new JSEncrypt();
+    crypt.setKey(publicKey);
+    return crypt.encrypt(data).toString();
 };
 
-export const generateEncryptionKey = (clientPublicKey: number, privateKey: number, bigPrime: number) => {
-  encryptionKey = expmod(clientPublicKey, privateKey, bigPrime).toString();
-};
-
-function expmod(base: number, key: number, mod: number) {
-  return (Math.pow(base, key)) % mod;
-}
-
-export const getPublicKey = (): string => {
-  return publicKey;
-};
-
-export const getEncryptionKey = (): string => {
-  return encryptionKey;
-};
-
-export const encryptData = (data: string): string => {
-  return CryptoJS.AES.encrypt(data, encryptionKey).toString();
-};
-
-export const decryptData = (data: string): string => {
-  return CryptoJS.AES.decrypt(data, encryptionKey).toString(CryptoJS.enc.Utf8);
+export const decryptData = (privateKey: string, data: string): string => {
+    const crypt = new JSEncrypt();
+    crypt.setKey(privateKey);
+    return crypt.decrypt(data).toString();
 };
 
 
